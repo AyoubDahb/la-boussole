f44f60d1b31d2e70943f04d67564e4fe2c9a5737
Aydahb <ayoub.dahbi.sio@gmail.com>
2026-05-07 11:56:48 +0200

    ajout du local storage pour la checklist

--- src/pages/Checklist.jsx
+++ src/pages/Checklist.jsx
@@ -1,42 +1,50 @@
-import { useState } from 'react'
-import  taches from '../data/Checklist.json'
-import CheckItem from '../components/CheckItem';
+import { useState, useEffect } from "react";
+import taches from "../data/Checklist.json";
+import CheckItem from "../components/CheckItem";
 
 function Checklist() {
+  const [checkedTaches, setCheckedTaches] = useState(() => {
+    var sauvegarde = localStorage.getItem("checkStore");
 
-    const [checkedTaches, setCheckedTaches] = useState(() =>{     
-      
-      const depart = {}
-      taches.forEach(tache =>{
+    if (sauvegarde != null) {
+      return JSON.parse(sauvegarde);
+    } else {
+      const depart = {};
+      taches.forEach((tache) => {
+        depart[tache.id] = false;
+      });
+      return depart;
+    }
+  });
+      useEffect(() => {
+      localStorage.setItem("checkStore", JSON.stringify(checkedTaches));
+    }, [checkedTaches]);
 
-      depart[tache.id] = false
+  const basculeTache = (id) => {
+    setCheckedTaches((precedent) => ({
+      ...precedent, // copie tout l'objet
+      [id]: !precedent[id], // inverse juste la valeur de l'id cliqué
+    }));
 
-    })
-    return depart
-  }); 
 
-    const basculeTache = (id) => {
+  };
 
-      setCheckedTaches(precedent => ({
-    ...precedent,       // copie tout l'objet
-    [id]: !precedent[id] // inverse juste la valeur de l'id cliqué
-    }))
-};
 
-console.log(JSON.stringify(checkedTaches))
-return (
-  <>
-    <ul className='checklist'>
-      {taches.map((tache) => (
-      <CheckItem
-        key={tache.id}        
-        tache={tache}
-        cochee={checkedTaches[tache.id]}  
-        bascule={basculeTache}/>
-      ))}
-    </ul>
-  </>
-);
+  console.log(JSON.stringify(checkedTaches));
+  return (
+    <>
+      <ul className="checklist">
+        {taches.map((tache) => (
+          <CheckItem
+            key={tache.id}
+            tache={tache}
+            cochee={checkedTaches[tache.id]}
+            bascule={basculeTache}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default Checklist
+export default Checklist;
--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,14 +1,13 @@
 import Button from '../components/button'
 
 function Plan() {
-
   return (
-    <>
+    <div className="plan-page">
       <div className="page-content">
         <h1>Plan du lycée</h1>
       </div>
-    <Button />
-    </>
+      <Button />
+    </div>
   )
 }
 
